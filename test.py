b7ad1f8259e52d6a22c2e11e041b67466758cab0
SharifMJahid <S398772@students.cdu.edu.au>
2026-01-09 19:52:52 +0930

    new user input taken

--- test.py
+++ test.py
@@ -2,3 +2,16 @@ import turtle
 
 # Global bounds
 min_x = max_x = min_y = max_y = 0
+import turtle
+
+# Global bounds
+min_x = max_x = min_y = max_y = 0
+
+
+def update_bounds():
+    global min_x, max_x, min_y, max_y
+    x, y = turtle.position()
+    min_x = min(min_x, x)
+    max_x = max(max_x, x)
+    min_y = min(min_y, y)
+    max_y = max(max_y, y)
